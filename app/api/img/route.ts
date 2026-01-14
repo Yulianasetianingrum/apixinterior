1f792cab1e6627548e371b0b76430147d242b49a
Yuliana Setianingrum <yulianasetianingrum123@gmail.com>
2026-01-15 02:26:24 +0700

    Enable content-length header for image details

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -33,6 +33,7 @@ export async function GET(request: Request) {
     return new NextResponse(fileBuffer, {
         headers: {
             'Content-Type': contentType,
+            'Content-Length': fileBuffer.length.toString(),
             'Cache-Control': 'public, max-age=31536000, immutable',
         },
     });
